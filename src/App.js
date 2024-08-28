ca3f51293843034cc054a47bdbc352787fdd5dcc
Shubham Yadav <shubhamyadav97660@gmail.com>
2024-08-28 12:07:37 +0530

    structure and logic of the tictactoe game completed successfull

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Square } from "./components/Square.jsx";
+import { calculateWinner } from "./utility/calculateWinner.js";
 
 function App() {
+  const [matrix, setMatrix] = useState(Array(9).fill(null));
+  const [isXSeleted, updateSelected] = useState(null);
+  function handleClick(i) {
+    if (matrix[i] || calculateWinner(matrix)) {
+      return;
+    }
+    let newMatrix = [...matrix];
+    if (isXSeleted) {
+      newMatrix[i] = "X";
+    } else {
+      newMatrix[i] = "O";
+    }
+    updateSelected(!isXSeleted);
+    setMatrix(newMatrix);
+  }
+  const winner = calculateWinner(matrix);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+    console.log(status);
+  } else {
+    status = "Next player: " + (isXSeleted ? "X" : "O");
+    console.log(status);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div style={{ fontSize: "3rem" }}>{status}</div>
+        <div className="gameContainer">
+          <Square value={matrix[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={matrix[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={matrix[2]} onSquareClick={() => handleClick(2)} />
+          <Square value={matrix[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={matrix[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={matrix[5]} onSquareClick={() => handleClick(5)} />
+          <Square value={matrix[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={matrix[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={matrix[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/utility/calculateWinner.js
@@ -0,0 +1,20 @@
+export const calculateWinner = (matrix) => {
+  const winner = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+
+  for (let i = 0; i < winner.length; i++) {
+    const [a, b, c] = winner[i];
+    if (matrix[a] && matrix[a] == matrix[b] && matrix[b] == matrix[c]) {
+      return matrix[a];
+    }
+  }
+  return null;
+};
